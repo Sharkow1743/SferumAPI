e9db484f6a9593edbda5daf4f199202206749e25
Sharkow1743 <sharkow1743@gmail.com>
2025-07-14 14:33:00 +0700

    Sepparated md parser

--- SferumBridge/sferum_api.py
+++ SferumBridge/sferum_api.py
@@ -1,40 +1,8 @@
 import requests
 from random import randint
-import re
 import json
+from .md import parse_markdown
 
-def parse_markdown(text):
-    """Parses markdown style text to a structured format for later usage.
-
-    Args:
-        text (str): The markdown formatted text.
-
-    Returns:
-        tuple: A tuple containing:
-            - list: List of formatting information (type, offset, length)
-            - str: Text without markdown formatting.
-    """
-    result = []
-    
-    def process_match(match, md_type):
-        format = {
-            "type": md_type,
-            "offset": match.start(),
-            "length": len(match.group(1)),
-        }
-        if md_type == 'url':
-            format['url'] = match.group(2)
-
-        result.append(format)
-
-        return match.group(1)
-    
-    text = re.sub(r'\[(.*?)\]\((?P<url>.*?)\)', lambda m: process_match(m, "url"), text)
-    text = re.sub(r'\*(.*?)\*', lambda m: process_match(m, "bold"), text)
-    text = re.sub(r'/(.*?)/', lambda m: process_match(m, "italic"), text)
-    text = re.sub(r'~(.*?)~', lambda m: process_match(m, "underline"), text)
-    
-    return result, text.strip()
 
 class SferumAPI:
     def __init__(self, remixdsid):
